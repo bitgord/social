e2488bfb4bf5023be51f73389eccda8b71fa8409
bitgord <michael.l.gord@gmail.com>
2016-07-12 19:42:38 -0400

    upload

---
+++ client/lib/accounts.js
@@ -0,0 +1,74 @@
+AccountsTemplates.configure({
+	hideSignInLink: true,
+	hideSignUpLink: true,
+	texts: {
+		title: {
+			signIn: 'Login',
+			signUp: 'Join the Social Network'
+		}
+	},
+	onSubmitHook: () => { FlowRouter.go('home'); },
+	onLogoutHook: () => { FlowRouter.go('home'); }
+});
+
+/**
+	* Helper function to generate an array of objects, like
+	* generationDates(1,2) returns
+	* [ { text: '1', value: '1'}, { text: '2', value: '2' } ]
+	*/
+let generateDates = (startDate, endDate) => {
+	let values = [];
+	for (startDate; startDate <= endDate; startDate++) {
+		let object = { text: String(startDate), value: String(startDate) };
+		values.push(object);
+	}
+	return values;
+};
+	
+AccountsTemplates.addFields([
+		{
+			_id: 'firstName',
+			type: 'text',
+			placeholder: 'Please enter your first name',
+			required: true,
+			displayName: 'First name'
+
+		},
+		{
+			_id: 'lastname',
+			type: 'text',
+			placeholder: 'Please enter your last name',
+			required: true,
+			displayName: 'Last name'
+
+		},
+		{
+			_id: 'username',
+			type: 'text',
+			placeholder: 'Please enter your  username',
+			required: true,
+			displayName: ' Username'
+
+		},
+		{
+			_id: 'dayOfBirth',
+			type: 'select',
+			required: true,
+			displayName: 'Day of birth',
+			select: generatedDates(1,31)
+		},
+		{
+			_id: 'monthOfBirth',
+			type: 'select',
+			required: true,
+			displayName: 'Month of birth',
+			select: generatedDates(1,12)
+		},
+		{
+			_id: 'yearOfBirth',
+			type: 'select',
+			required: true,
+			displayName: 'Month of birth',
+			select: generatedDates(1980,2000)
+		}
+	]);
