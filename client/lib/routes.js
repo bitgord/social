b479490d31a2265083bde700cb646562edfcd6a1
bitgord <michael.l.gord@gmail.com>
2016-07-18 23:32:19 -0400

    Add files via upload

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -6,18 +6,50 @@ FlowRouter.route('/', {
   }
 });
 
+/**
+ * Check if a user is logged in to restrict access to signup and login
+ */
+let loggedInCheck = [
+  () => { if (Meteor.userId()) { FlowRouter.go('dashboard'); } }
+];
+
 /** Define the signup route */
 FlowRouter.route('/signup', {
-	name: 'signup',
-	action () {
-		BlazeLayout.render('fullLayout', { main: 'signup' });
-	}
+  name: 'signup',
+  action () {
+    BlazeLayout.render('fullLayout', { main: 'signup' });
+  },
+  triggersEnter: loggedInCheck
 });
 
 /** Define the login route */
 FlowRouter.route('/login', {
-	name: 'login',
-	action () {
-		BlazeLayout.render('fullLayout', { main: 'login' });
-	}
+  name: 'login',
+  action () {
+    BlazeLayout.render('fullLayout', { main: 'login' });
+  },
+  triggersEnter: loggedInCheck
+});
+
+/**
+ * Define the userRoutes group, in order to restrict access for users who 
+ * are not logged in
+ */
+let userRoutes = FlowRouter.group({
+  prefix: '/user',
+  name: 'userRoutes',
+  triggersEnter: [
+    () => { if (!Meteor.userId()) { FlowRouter.go('home'); } }
+  ]
+});
+
+/** Define the dashboard route */
+userRoutes.route('/dashboard', {
+  name: 'dashboard',
+  action () {
+    BlazeLayout.render('mainLayout', {
+      navigation: 'userNav',
+      content: 'dashboard'
+    });
+  }  
 });
